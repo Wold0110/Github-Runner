4f51bfea2faceb2043bef2675e351a73d337908b
Wold0110 <walter20020110@gmail.com>
2023-08-16 11:20:09 +0200

    run config only once

---
+++ Run.ps1
@@ -0,0 +1,2 @@
+docker run --privileged -e https_proxy=http://165.225.200.15:80 -e http_proxy=http://165.225.200.15:80 `
+-e GITHUB_URL=https://github.com/Wold0110/LDS_ASP.NET -e GITHUB_TOKEN=AGHI5RM765K7FS54FCCLIF3E3SOYK wolf0110/github-runner
--- docker.sh
+++ docker.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
+
+CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
+if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
+    touch $CONTAINER_ALREADY_STARTED
+	echo "-- First container startup - config in progress --"
+    ./config.sh --url $GITHUB_URL --token $GITHUB_TOKEN
+else
+    echo "-- Not first container startup --"
+fi
+
+#always run
 sudo dockerd & disown
-./config.sh --url $GITHUB_URL --token $GITHUB_TOKEN
 ./run.sh
-#service docker start
